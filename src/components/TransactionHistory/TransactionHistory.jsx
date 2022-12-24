3b85d2e3f5e5a7323fde422425b3df57c04e2222
Marina Zalozna <marina.zalozna@gmail.com>
2022-12-24 19:04:33 +0100

    Correct HW

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,32 +1,31 @@
-import React from "react";
-import PropTypes from "prop-types";
-import css from "./TransactionHistory.module.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
 
-export const TransactionHistory = ({ transactions }) => {
+export const TransactionHistory = ({ items }) => {
   return (
     <div className={css.wrapper}>
       <table className={css.transaction_history}>
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
-      <tbody>
-      {transactions.map((event) => {
-          return (
-          <tr key={event.id}>
-            <td>{event.type}</td>
-            <td>{event.amount}</td>
-            <td>{event.currency}</td>
-          </tr>)
-      })}
-      </tbody>;
-    </table>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(event => {
+            return (
+              <tr key={event.id}>
+                <td>{event.type}</td>
+                <td>{event.amount}</td>
+                <td>{event.currency}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-    
-   
   );
 };
 
